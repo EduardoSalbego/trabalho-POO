dc0b53e231b47f0d786348ffff54e30d49554f92
ddsalb <eduaro.salbego@gmail.com>
2023-01-16 21:08:36 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Item {
+    
+    private double quantidade;
+    private double preco;
+    private Produto produto;
+
+    public Item (Produto produto, double quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.preco = produto.getPreco();
     }
+    
+    public double getTotal(){
+        return preco*quantidade;
+    }
+
+    public double getQuantidade(){return quantidade;}
+    public String getProduto(){return produto.getNome();}
 }
