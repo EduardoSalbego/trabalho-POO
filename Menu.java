e784c9559e9fe852fcc64aaa9ac8de31b23d24df
ddsalb <eduaro.salbego@gmail.com>
2023-01-18 23:03:03 +0000

    criei a classe menu

---
+++ Menu.java
@@ -0,0 +1,17 @@
+public class Menu {
+    
+}
+public class Menu {
+
+    private ListaNotasFiscais lnf;
+    private ListaProdutos lp;
+
+    public Menu(ListaNotasFiscais lnf, ListaProdutos lp){
+        this.lnf = lnf;
+        this.lp = lp;
+    }
+
+    public void show() {
+
+    }
+}
