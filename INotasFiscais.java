15ce62e96f3e6bb237999b074e2ad3427d863feb
ddsalb <eduaro.salbego@gmail.com>
2023-01-18 23:08:04 +0000

    testando

--- INotasFiscais.java
+++ INotasFiscais.java
@@ -5,7 +5,7 @@
 public interface INotasFiscais {
             
     /**
-     * Adiciona uma nota fiscal.
+     * Adiciona uma nota fiscal. hha
      * @param nf Nota fiscal a ser adicionada.
      * @throws java.lang.Exception Lança exceção quando a nota fiscal não pode ser adicionada.
      */
